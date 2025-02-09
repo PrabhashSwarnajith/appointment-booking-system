148dcdb4ccc3bae9a499bb4d4d5307905136d52b
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-09 18:56:57 +0530

    Add initial setup for backend

---
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/AppointmentSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.alphacode.appointment_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentSystemApiApplication.class, args);
+	}
+
+}
---
+++ appointment-system-api/src/test/java/com/alphacode/appointment_system_api/AppointmentSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphacode.appointment_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
