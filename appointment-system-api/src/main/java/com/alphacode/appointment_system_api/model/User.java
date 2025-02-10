7492c1051b5af5f1d437ab1bbf5514f9e6884d6c
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-10 20:40:23 +0530

    Add User model with fields for id, name, email, password, and role

---
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/model/User.java
@@ -0,0 +1,35 @@
+package com.alphacode.appointment_system_api.model;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Role role;
+}
