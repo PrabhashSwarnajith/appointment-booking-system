29d6ef16036fd7c6749a06ac1e8f710093b6efd8
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-10 21:16:37 +0530

    Change User ID type from String to Long for consistency with JPA annotations

--- appointment-system-api/src/main/java/com/alphacode/appointment_system_api/model/User.java
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/model/User.java
@@ -23,7 +23,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     
     @Column(nullable = false)
     private String name;
