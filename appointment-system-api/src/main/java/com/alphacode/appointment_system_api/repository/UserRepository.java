f7122c159206ebd8541b1302d77ca43bfcbe10af
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-10 20:42:42 +0530

    Add @Repository annotation to UserRepository interface

--- appointment-system-api/src/main/java/com/alphacode/appointment_system_api/repository/UserRepository.java
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.alphacode.appointment_system_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alphacode.appointment_system_api.model.User;
 
