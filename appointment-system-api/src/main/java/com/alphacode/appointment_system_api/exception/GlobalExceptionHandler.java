8b8d3a9881a22963dc0e081a1f88b98dc97c2a29
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-13 01:25:07 +0530

    Refactor JWT authentication filter to improve error handling and logging; update global exception handler for consistency

--- appointment-system-api/src/main/java/com/alphacode/appointment_system_api/exception/GlobalExceptionHandler.java
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/exception/GlobalExceptionHandler.java
@@ -5,14 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex) {
-        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
+    
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
