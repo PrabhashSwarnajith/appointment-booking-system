09a4c3450f40e70a0362366a2b1743d45afaa631
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-13 01:25:22 +0530

    Add Appointment entity and DTO for appointment management

---
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/dto/AppointmentDTO.java
@@ -0,0 +1,29 @@
+package com.alphacode.appointment_system_api.dto;
+
+
+import java.time.LocalDate;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AppointmentDTO {
+    private Long id;
+    private Long slot_id;
+    private String user_name;
+    private String user_contact;
+    private String user_email;
+    private LocalDate date;
+    private String time;
+    private String status;
+    private String message;
+}
---
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/model/Appointment.java
@@ -0,0 +1,44 @@
+package com.alphacode.appointment_system_api.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "appointments")
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false) // Foreign key to User
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "slot_id", nullable = false) // Foreign key to Slot
+    private Slot slot;
+
+}
