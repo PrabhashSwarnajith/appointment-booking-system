0b5f62962995b1e48f0e6c0ad35d219c207bca5c
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-12 18:34:34 +0530

    Refactor User model to change name to username and add appointments relationship

--- appointment-system-api/src/main/java/com/alphacode/appointment_system_api/model/User.java
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/model/User.java
@@ -2,11 +2,14 @@ package com.alphacode.appointment_system_api.model;
 
 
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,8 +28,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Column(nullable = false)
-    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
 
     @Column(nullable = false , unique = true)
     private String email;
@@ -36,4 +39,8 @@ public class User {
 
     @Column(nullable = false)
     private Role role;
+
+    @OneToMany(mappedBy = "userId")
+    private List<Appointment> appointments;
+
 }
