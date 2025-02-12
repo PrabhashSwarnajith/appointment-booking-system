914c4e7599e9a19093a47e65cd90b68de7b5b16f
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-12 18:35:29 +0530

    Add SlotsController to handle retrieval of available slots by date

---
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/controller/SlotsController.java
@@ -0,0 +1,29 @@
+package com.alphacode.appointment_system_api.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alphacode.appointment_system_api.dto.SlotDTO;
+import com.alphacode.appointment_system_api.service.SlotsService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping ("/api/")
+@AllArgsConstructor
+public class SlotsController {
+
+    private final SlotsService slotsService;
+
+    @GetMapping(value = "/slots")
+    public ResponseEntity<List<SlotDTO>> getSlots(@RequestParam LocalDate date) {
+        return ResponseEntity.ok(slotsService.getSlots(date));
+    }
+
+}
