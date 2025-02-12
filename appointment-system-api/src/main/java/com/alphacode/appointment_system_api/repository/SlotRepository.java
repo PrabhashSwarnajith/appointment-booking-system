48ef35942f311aa9cc053f44ee31840546e74ec1
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-12 18:37:57 +0530

    Add SlotRepository for managing appointment slots in the database

---
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/repository/SlotRepository.java
@@ -0,0 +1,20 @@
+package com.alphacode.appointment_system_api.repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alphacode.appointment_system_api.model.Slot;
+
+@Repository
+public interface SlotRepository extends JpaRepository<Slot, Long> {
+
+    boolean existsByDateAndStartTimeAndEndTime(LocalDate date, LocalTime startTime, LocalTime endTime);
+    
+    List<Slot> findByDate(LocalDate date);
+
+    boolean existsByDate(LocalDate date); 
+}
