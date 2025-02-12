dfad7acd96b3c18daaec7ba5f83054937f746826
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-12 18:37:21 +0530

    Add JWT authentication components: UserPrinciple, JwtAuthenticationFilter, and JwtService

---
+++ appointment-system-api/src/main/java/com/alphacode/appointment_system_api/security/JwtService.java
@@ -0,0 +1,81 @@
+package com.alphacode.appointment_system_api.security;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class JwtService {
+
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    private static final long EXPIRATION_TIME = 30 * 60 * 1000;
+    
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String generateToke(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        try{
+            return Jwts.builder()
+            .claims()
+            .add(claims)
+            .subject(userDetails.getUsername())
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .and()
+            .signWith(getSignInKey())
+            .compact();
+        }catch(Exception e){
+            log.error("Error generating token for user: {}", userDetails.getUsername(), e);
+            throw new RuntimeException("Error generating JWT token", e);
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
