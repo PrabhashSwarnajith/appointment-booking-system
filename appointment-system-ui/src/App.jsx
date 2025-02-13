8dcf54123175fce1a5e09475896eafc6c0218fc4
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-14 02:24:27 +0530

    Implement routing and authentication checks in App component

--- appointment-system-ui/src/App.jsx
+++ appointment-system-ui/src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import AvailableSlots from './pages/AvailableSlots';
+import BookAppointment from './pages/BookAppointment';
+import Home from './pages/Home';
+import ViewAppointments from './pages/ViewAppointments';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import AuthService from '../src/services/AuthServise';
+import Navbar from './components/Navbar';
 
 function App() {
-  
+  const isAuthenticated = AuthService.isAuthenticated();
 
   return (
-    <>
-      <h1>Appointment System</h1>
-    </>
-  )
+    <BrowserRouter>
+      {isAuthenticated && <Navbar />} {/* Show Navbar only if logged in */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        <Route path="/available-slots" element={isAuthenticated ? <AvailableSlots /> : <Navigate to="/login" />} />
+        <Route path="/book" element={isAuthenticated ? <BookAppointment /> : <Navigate to="/login" />} />
+        <Route path="/appointments" element={isAuthenticated ? <ViewAppointments /> : <Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
